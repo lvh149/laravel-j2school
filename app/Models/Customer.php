4bde9e301ac9f6b73621881b7210d0ef2d037f1e
TamNguyen <43509906+TamNguyen231173@users.noreply.github.com>
2022-07-21 15:41:49 +0700

    update: gender customer

--- app/Models/Customer.php
+++ app/Models/Customer.php
@@ -13,8 +13,18 @@ class Customer extends Model
 
     public function getGenderNameAttribute(): string
     {
-        return ($this->gender === 0) ? 'Nữ' : 'Nam';
+        $genderValue = $this->gender;
+        if($genderValue === 0) {
+            $gender = 'Nữ';
+        } else if ($genderValue === 1) {
+            $gender = 'Nam';
+        } else {
+            $gender = 'Khác';
+        }
+
+        return $gender;
     }
+
     public function getAgeAttribute(): int
     {
         return date_diff(date_create($this->birth_date), date_create())->y;
