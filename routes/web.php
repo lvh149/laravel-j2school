f0d24fa1f921f6fe735d6c8a48537a1e45eb1bb0
TamNguyen <43509906+TamNguyen231173@users.noreply.github.com>
2022-07-21 15:38:16 +0700

    add search doctor

--- routes/web.php
+++ routes/web.php
@@ -9,38 +9,38 @@ use App\Http\Controllers\DoctorController;
 use App\Http\Controllers\SpecialistController;
 use App\Http\Controllers\TimeDoctorController;
 use Illuminate\Support\Facades\Route;
+
 // Route user
-Route::get('/login', [AuthController::class, 'login']);
-Route::get('/register', [AuthController::class, 'register']);
-Route::get('/booking', [CustomerController::class, 'booking'])->name('user.booking');
-Route::get('/',  function () {
-    return view('layout.master');
+route::group(['prefix' => 'user'], function () {
+    Route::get('login', [AuthController::class, 'userLogin'])->name('user.login');
+    Route::get('register', [AuthController::class, 'userRegister'])->name('user.register');
+    Route::get('booking', [CustomerController::class, 'booking'])->name('booking.index');
+    Route::get('doctor', [DoctorController::class, 'doctor'])->name('doctor');
+    Route::get('search', [DoctorController::class, 'search'])->name('doctor.search');
 });
 
-
+Route::get('/', function () {
+    return view('user.layout.homepage');
+})->name('user.homepage');
 
 //Route admin
-
-route::group(['prefix'=>'admin'],function (){
-Route::get('login', [AuthController::class, 'adminLogin'])->name('admin.login');
-Route::get('appointment',[AppointmentController::class,'index'])->name('appointment.index');
-Route::resource('/specialist', SpecialistController::class)->except([
-    'show',
-]);
-Route::resource('/doctor', DoctorController::class)->except([
-    'show',
-]);
-Route::resource('/customer', CustomerController::class)->except([
-    'show',
-]);
-Route::resource('/time_doctor', TimeDoctorController::class)->except([
-    'show',
-]);
-
+route::group(['prefix' => 'admin'], function () {
+    Route::get('login', [AuthController::class, 'adminLogin'])->name('admin.login');
+    Route::get('appointment', [AppointmentController::class, 'index'])->name('appointment.index');
+    Route::resource('/specialist', SpecialistController::class)->except([
+        'show',
+    ]);
+    Route::resource('/doctor', DoctorController::class)->except([
+        'show',
+    ]);
+    Route::resource('/customer', CustomerController::class)->except([
+        'show',
+    ]);
+    Route::resource('/time_doctor', TimeDoctorController::class)->except([
+        'show',
+    ]);
 });
 
 Route::get('/admin', function () {
     return view('admin.layout.master');
 })->name('home');
-
-
