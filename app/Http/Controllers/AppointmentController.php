b9d297861188c2dea50e8114508942200e5832ca
TamNguyen <43509906+TamNguyen231173@users.noreply.github.com>
2022-08-17 03:32:36 +0700

    update(select time): user- appointment- create

--- app/Http/Controllers/AppointmentController.php
+++ app/Http/Controllers/AppointmentController.php
@@ -8,8 +8,6 @@ use App\Models\Appointment;
 use App\Http\Requests\StoreAppointmentRequest;
 use App\Models\Time;
 use App\Models\Time_doctor;
-use Carbon\Carbon;
-use App\Http\Requests\Appointment\StoreRequest;
 use App\Http\Requests\UpdateAppointmentRequest;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
@@ -40,32 +38,37 @@ class AppointmentController extends Controller
 
     public function create(doctor $doctor)
     {
-        $current_time = Carbon::now();
         $time_doctors = (new time_doctor())->query()
-            ->with('appointment:time_doctor_id,status')
-            ->with('doctor:id,name')
             ->with('time:id,date,time_start,time_end')
+            ->with('appointment:time_doctor_id,status')
             ->where('doctor_id','=',$doctor->id)
-            ->latest('id')
             ->get();
-        $specialists = Specialist::query()->get();
+        $specialists = (new specialist())->query()->get();
         return view('user.appointment.create', [
             'doctor' => $doctor,
             'specialists' => $specialists,
             'time_doctors' => $time_doctors,
-            'current_time' => $current_time,
         ]);
 
     }
 
-    public function search(Request $request)
+    public function selectTime(Request $request)
     {
-         $time_doctors = (new time())->query()
-            ->where('date','=',$request->date)
-             ->whererelation('time_doctor.doctor','id','=',$request->doctor_id)
-             ->orderBy('time_start')
-            ->get();
-         return $time_doctors;
+//         $time_doctors = (new time())->query()
+//            ->where('date','=',$request->date)
+//             ->whererelation('time_doctor.doctor','id','=',$request->doctor_id)
+//             ->orderBy('time_start')
+//            ->get();
+            $time_doctors = (new time_doctor())->query()
+                ->where('doctor_id', '=', $request->doctor_id)
+                ->join('times', 'time_doctors.time_id', '=', 'times.id')
+                ->where('date', '=', $request->date)
+                ->leftJoin('appointments', 'time_doctors.id', '=', 'appointments.time_doctor_id')
+                ->select('time_doctors.id','status', 'time_start', 'time_end')
+                ->orderBy('time_start')
+                ->get();
+
+            return $time_doctors;
     }
 
     public function update(Request $request, Appointment $appointment)
