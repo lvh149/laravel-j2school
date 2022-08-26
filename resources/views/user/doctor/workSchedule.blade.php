d7125a171675239653f7e89cee64b5cb8bf27744
TamNguyen <43509906+TamNguyen231173@users.noreply.github.com>
2022-08-26 18:44:18 +0700

    update ui work schedule doctor

--- resources/views/user/doctor/workSchedule.blade.php
+++ resources/views/user/doctor/workSchedule.blade.php
@@ -1,1 +1,28 @@
-<p>Hello</p>
+@extends('user.layout.master')
+@section('content')
+    <div class="row">
+        <div class="col-md-12">
+            <div id="calendar"></div>
+        </div>
+    </div>
+
+@endsection
+@push('js')
+<script src="https://cdn.jsdelivr.net/npm/fullcalendar@5.11.3/main.min.js"></script>
+
+<script>
+    document.addEventListener('DOMContentLoaded', function() {
+        var calendarEl = document.getElementById('calendar');
+        var calendar = new FullCalendar.Calendar(calendarEl, {
+            events: '{{ route('get_doctor', \Illuminate\Support\Facades\Auth::id()) }}',
+            initialView: 'dayGridMonth',
+            headerToolbar: {
+                left: 'prev,next today',
+                center: 'title',
+                right: 'dayGridMonth,timeGridWeek,listWeek'
+            }
+        });
+        calendar.render();
+    });
+</script>
+@endpush
