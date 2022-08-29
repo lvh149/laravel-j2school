8a76c93ef7408fb1ab8de7f2c051e21c52edf5e3
TamNguyen <43509906+TamNguyen231173@users.noreply.github.com>
2022-08-29 11:20:38 +0700

    add getAndCache function

--- app/Models/config.php
+++ app/Models/config.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use App\Enums\SystemCacheKeyEnum;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -9,4 +10,25 @@ class config extends Model
 {
     use HasFactory;
     public $timestamps = false;
+
+    public function getAndCache($isPublic): array
+    {
+
+        return cache()->remember(
+            SystemCacheKeyEnum::CONFIGS . $isPublic,
+            86000 * 30,
+            function () use ($isPublic) {
+                $data = self::query()
+                    ->where('is_public', $isPublic)
+                    ->get();
+                $arr = [];
+
+                foreach ($data as $each) {
+                    $arr[$each->key] = $each->value;
+                }
+
+                return $arr;
+            }
+        );
+    }
 }
