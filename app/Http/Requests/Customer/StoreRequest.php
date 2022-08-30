14bc346418c033738980e5af463e840b50d28e18
TamNguyen <43509906+TamNguyen231173@users.noreply.github.com>
2022-08-30 14:58:32 +0700

    update store request customer

--- app/Http/Requests/Customer/StoreRequest.php
+++ app/Http/Requests/Customer/StoreRequest.php
@@ -2,45 +2,89 @@
 
 namespace App\Http\Requests\Customer;
 
+use App\Models\Specialist;
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
 
 class StoreRequest extends FormRequest
 {
-    public function authorize()
+    public function authorize(): bool
     {
         return true;
     }
 
-    public function rules()
+    public function rules(): array
     {
         return [
             'name_booking' => [
+                'nullable',
                 'string',
             ],
             'phone_booking' => [
+                'nullable',
                 'string',
             ],
             'name_patient' => [
+                'bail',
                 'required',
                 'string',
             ],
             'phone_patient' => [
+                'bail',
                 'required',
-                'string',
+                'numeric',
+                'unique:customers,phone_patient'
             ],
             'email' => [
+                'bail',
                 'required',
                 'email',
+                'unique:customers,email'
             ],
             'gender' => [
                 'required',
                 'boolean',
             ],
             'birth_date' => [
+                'bail',
                 'required',
                 'date',
                 'before:today',
             ],
+            'description' => [
+                'bail',
+                'required',
+                'max:500',
+            ],
+            'time_doctor_id' => [
+                'required',
+                'numeric',
+            ],
+            'price' => [
+                'required',
+                'numeric',
+    ]
+        ];
+    }
+
+    public function messages(): array
+    {
+        return [
+            'required' => ':attribute bắt buộc phải điền',
+            'unique' => ':attribute đã được dùng',
+            'numeric' => ':attribute phải là số',
+            'max: 500' => ':attribute tối đa 500 kí tự',
+            'before:today' => ':attribute không hợp lệ',
+        ];
+    }
+
+    public function attributes(): array
+    {
+        return [
+            'name_patient' => 'Tên bệnh nhân',
+            'phone_patient' => 'Số điện thoại',
+            'birth_date' => 'Ngày sinh',
+            'description' => 'Tình trạng bệnh nhân',
         ];
     }
 }
