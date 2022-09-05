7a902da8d8e7e4bfb1a2ee9596558b9507331759
TamNguyen <43509906+TamNguyen231173@users.noreply.github.com>
2022-09-05 13:18:38 +0700

    fix create customer

--- app/Http/Controllers/AppointmentController.php
+++ app/Http/Controllers/AppointmentController.php
@@ -31,7 +31,7 @@ class AppointmentController extends Controller
             ->where('status', '=', $request->status)
             ->latest('updated_at')
             ->paginate();
-        $appointments->appends(['status'=>$request->status]);
+        $appointments->appends(['status' => $request->status]);
         return view('admin.appointment.index', [
             'appointments' => $appointments,
         ]);
@@ -39,7 +39,7 @@ class AppointmentController extends Controller
 
     public function create($doctor_id)
     {
-        $doctor= doctor::query()
+        $doctor = doctor::query()
             ->with('specialist')
             ->find($doctor_id);
         $specialists = (new specialist())->query()->get();
@@ -51,16 +51,16 @@ class AppointmentController extends Controller
 
     public function selectTime(Request $request)
     {
-            $time_doctors = (new time_doctor())->query()
-                ->where('doctor_id', '=', $request->doctor_id)
-                ->join('times', 'time_doctors.time_id', '=', 'times.id')
-                ->where('date', '=', $request->date)
-                ->leftJoin('appointments', 'time_doctors.id', '=', 'appointments.time_doctor_id')
-                ->select('time_doctors.id','status', 'date', 'time_start', 'time_end')
-                ->orderBy('time_start')
-                ->get();
+        $time_doctors = (new time_doctor())->query()
+            ->where('doctor_id', '=', $request->doctor_id)
+            ->join('times', 'time_doctors.time_id', '=', 'times.id')
+            ->where('date', '=', $request->date)
+            ->leftJoin('appointments', 'time_doctors.id', '=', 'appointments.time_doctor_id')
+            ->select('time_doctors.id', 'status', 'date', 'time_start', 'time_end')
+            ->orderBy('time_start')
+            ->get();
 
-            return $time_doctors;
+        return $time_doctors;
     }
 
     public function update(UpdateRequest $request, Appointment $appointment)
